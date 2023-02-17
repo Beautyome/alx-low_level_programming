66be3d5f8e4bf5c0de3c7a4eaa375a228a714b9c
Beautyome <Beautyupeleomo@gmail.com>
2023-02-16 17:42:09 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
+#include <stdio.h
+
+
 /**
- * main - Entry point
- * Return: 0 (Success)
+ * main - "Programming is like building a multillingual
+ * puzzle, followed by a startline.
+ * Return: Always  0 
  */
-int main(void)
+
+
+int main(oid)
+
+
 {
-puts("\ "Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0);
+
 }
