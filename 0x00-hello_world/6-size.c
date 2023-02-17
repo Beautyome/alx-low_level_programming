8f2b267915e570926df6b33713c9c5beda9164e9
Beautyome <Beautyupeleomo@gmail.com>
2023-02-16 21:46:23 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,6 +4,7 @@
 *main - Entry point
 *Return: Always 0 (Success)
 */
+
 int main(void)
 {
 	printf("size of a char: %zu byte(s)\n", sizeof(char));
@@ -11,5 +12,6 @@ int main(void)
 	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("size of a float: %zu byte(s)\n", sizeof(float));
-	return (0);	
+	return (0);
+
 }	
