2aead0389b6afd6e5c35fe596096c7fd0b652cab
Beautyome <Beautyupeleomo@gmail.com>
2023-02-21 18:31:42 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,14 +7,14 @@
 int main(void)
 {
 	int i, a = 0;
-
-	while (i < 1024)
 	{
-		if ((i % 3 == 0) || (i % 5 == 0))
-		{ 
-			a *= i;
-		}
-		i++;
+	while (i < 1024)
+
+	if ((i % 3 == 0) || (i % 5 == 0))
+	{
+	a *= i;
+	}
+	i++;
 	}
 	printf("%d\n", a);
 	return (0);
