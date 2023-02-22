2476ba93a1a7262534659f6b8573097fc888b596
Beautyome <Beautyupeleomo@gmail.com>
2023-02-22 11:14:20 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	unsigned long int i;
-	unsigned long int bef = 1
+	unsigned long int bef = 1;
 	unsigned long int aft = 2;
 	unsigned long int l = 1000000000;
 	unsigned long int bef1;
@@ -35,11 +35,10 @@ int main(void)
 		printf(", %lu", aft1 + (aft2 / l));
 		printf("%lu", aft2 % l);
 		aft1 = aft1 + bef1;
-		aft1 = aft1 - bef1;
+		bef1 = aft1 - bef1;
 		aft2 = aft2 + bef2;
-		aft2 = aft2 - bef2;
+		bef2 = aft2 - bef2;
 	}
-	printf('\n');
-
+	printf("\n");
 	return (0);
 }
